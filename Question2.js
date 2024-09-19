e7bfd01e1c3c270106f811d30c4bb937e9cba9e8
manas1803 <manasmonupant@gmail.com>
2024-09-19 19:42:40 +0530

    2nd question completed

--- Question2.js
+++ Question2.js
@@ -1,3 +1,24 @@
 // Question create the following function cal(2).add(3).add(1).sub(3).mul(2).val()
 
+function cal(num){
+    return {
+        add:function(n){
+            num=num+n
+            return this;
+        },
+        sub:function(n){
+            num=num-n;
+            return this;
+        },
+        mul:function(n){
+            num=num*n;
+            return this;
+        },
+        val:function(){
+            return num;
+        }
+    }
+}
 
+const sol = cal(2).add(3).add(1).sub(3).mul(2).val()
+console.log(sol)
