42853c22fae0aaced06d9822a3d2bdcbdc6f7c33
manas1803 <manasmonupant@gmail.com>
2024-09-19 19:36:41 +0530

    1st question completed

---
+++ SetTimeoutOutput.js
@@ -0,0 +1,25 @@
+console.log(1);
+setTimeout(() => {console.log(2);}, 0);
+var promise1 = new Promise((resolve, reject) => {
+console.log(3);
+resolve('success');
+})
+promise1.then(() => {
+console.log(4);
+})
+console.log(5);
+
+/**  Solution
+callStack = [log(1),new Promise(callback=>log 3),]
+Macrotask queue = [setTimeout,] - this will remove last
+MicrotaskQueue = [Promise resolve] - Empty this first
+
+
+Output :- 
+1
+3
+5
+4
+2
+*/
+
